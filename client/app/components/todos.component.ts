0dd6a5cf76db32e49303d4c5682fbb2a6dcb4f87
Hsu <edittye379@gmail.com>
2017-01-24 02:48:12 +0800

    add && delete todo

--- client/app/components/todos.component.ts
+++ client/app/components/todos.component.ts
@@ -8,9 +8,37 @@ import { TodoService } from '../services/todo.service';
 })
 export class TodosComponent  {
 	todos: Todo[];
+	title: string;
+
 	constructor(private todoService: TodoService){
 		this.todoService.getTasks().subscribe(todos => {
 			this.todos = todos;
+		});
+	}
+
+	addTodo(event: Object) {
+		// event.preventDefault();
+		// console.log(this.title); // two way binding
+		let newTodo = {
+			title: this.title,
+			isDone: false
+		};
+		this.todoService.addTodo(newTodo).subscribe(todo => {
+			this.todos.push(newTodo);
+			this.title = '';
+		})
+	}
+
+	deleteTodo(objId: string) {
+		let todos = this.todos;
+		this.todoService.deleteTodo(objId).subscribe(data => {
+			if(data.n == 1) {
+				for (var i = 0; i < todos.length; ++i) {
+					if(todos[i]._id == objId) {
+						todos.splice(i, 1);
+					}
+				}
+			}
 		});
 	}
 }
--- client/app/services/todo.service.ts
+++ client/app/services/todo.service.ts
@@ -8,7 +8,17 @@ export class TodoService {
 		console.log('TodoService Load');
 	}
 	getTasks() {
-		return this.http.get('http://localhost:3000/api/todos')
+		return this.http.get('/api/todos')
 			.map(res => res.json())
+	}
+	addTodo(newTodo: Object) {
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+		return this.http.post('/api/todo', JSON.stringify(newTodo), { headers: headers })
+			.map(res => res.json());
+	}
+	deleteTodo(id: string) {
+		return this.http.delete('/api/todo/' + id)
+			.map(res => res.json());
 	}
 }
